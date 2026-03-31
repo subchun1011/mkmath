d729db4190ebc1c7213f5dc36c0186b827f2c88e
subchun <subchun1011@gmail.com>
2026-03-31 13:32:29 +0900

    git 배포 봇

--- kids-game/vite.config.js
+++ kids-game/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 핵심: 저장소 이름을 앞뒤에 슬래시와 함께 넣어주세요.
+  base: '/mkmath/', 
 })
